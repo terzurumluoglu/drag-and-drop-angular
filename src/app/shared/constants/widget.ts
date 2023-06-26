f5c238ae8a022fd096e55b8f999653afff562b41
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-06-26 12:03:55 +0300

    mock widget data

---
+++ src/app/shared/constants/widget.ts
@@ -0,0 +1,38 @@
+
+import {
+    AComponent,
+    BComponent,
+    CComponent,
+    DComponent,
+    EComponent,
+    FComponent
+} from "src/app/components";
+
+import { IWidget } from "../model";
+
+export const WIDGETS: IWidget[] = [
+    {
+        title: 'A',
+        component: AComponent
+    },
+    {
+        title: 'B',
+        component: BComponent
+    },
+    {
+        title: 'C',
+        component: CComponent
+    },
+    {
+        title: 'D',
+        component: DComponent
+    },
+    {
+        title: 'E',
+        component: EComponent
+    },
+    {
+        title: 'F',
+        component: FComponent
+    },
+];
