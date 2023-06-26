7ac6cf23f0ea182f8c20f64e5d5e1bd643a2560d
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-06-26 12:03:04 +0300

    edited base compnent

--- src/app/shared/components/base/base.component.ts
+++ src/app/shared/components/base/base.component.ts
@@ -1,9 +1,20 @@
 import { Directive } from '@angular/core';
+import { ContentService, SanitizerService } from '../../services';
+import { SafeHtml } from '@angular/platform-browser';
 
 @Directive({
   selector: 'app-base'
 })
 export abstract class BaseComponent {
+  constructor(
+    public sanitizerService: SanitizerService,
+    public contentService: ContentService
+  ) { }
+
+  getSafeContent = (componentName: string): SafeHtml => {
+    const content: string = this.contentService.getContent(componentName);
+    return this.sanitizerService.safeHtml(content);
+  }
   
 }
 
